13e50141931364a359bacbc46eb2675672faea71
catiaviana <catiaviana95@outlook.pt>
2024-04-23 15:15:22 +0100

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+import profile_img  from '../../assets/profile_img.png'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Hero = () => {
+    return (
+        <div id='home' className='hero'>
+            <img src={profile_img} alt="" />
+            <h1><span>I'm Cátia Fernandes,</span> a web developer.</h1>
+            <p>I'm a new fresh developer from Porto, Portugal.</p>
+            <div className="hero-action">
+                <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+                <div className="hero-resume">My resume</div>
+            </div>
+        </div>
+    )
+}
+export default Hero
